41d2f1add81eca5c644bb3979f9cc5682b143070
Deibson Cogo <deibsoncogo@outlook.com>
2024-12-30 20:01:54 -0300

    feat: criando uma classe de teste

---
+++ poo.py
@@ -0,0 +1,12 @@
+class People:
+  def __init__(self, name, age) -> None:
+    self.name = name
+    self.age = age
+
+  def greeting(self):
+    return f"Olá, meu nome é {self.name} e eu tenho {self.age} anos"
+
+people1 = People("Alice", 30)
+
+messagePeople1 = people1.greeting()
+print(messagePeople1)
